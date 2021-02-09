b3d9ebba30ae021dbfe024035500900ac311177b
albatrosary <fumio.sagawa@gmail.com>
2021-02-09 11:05:47 +0900

    change to ormconfig.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,18 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-// import * as ormconfig from './config/ormconfig';
+import * as ormconfig from './config/ormconfig';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './entity/User';
 
 export function DatabaseOrmModule(): DynamicModule {
-  return TypeOrmModule.forRoot({
-    "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
-    "username": "root",
-    "password": "password",
-    "database": "TestDB",
-    "synchronize": true,
-    "logging": false,
-    "entities": [
-      "dist/entity/**/*.js"
-    ],
-    "migrations": [
-      "dist/migrations/**/*.js"
-    ],
-    "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migration"
-    }
-  });
+  return TypeOrmModule.forRoot(ormconfig);
 }
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([User])],
+  imports: [DatabaseOrmModule(), TypeOrmModule.forFeature([User])],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/ormconfig.ts
@@ -0,0 +1,27 @@
+import {ConnectionOptions} from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'password',
+  database: 'TestDB',
+  synchronize: true,
+  logging: false,
+  entities: [
+    "dist/entity/**/*.js"
+  ],
+  migrations: [
+    "dist/migration/**/*.js"
+  ],
+  subscribers: [
+    "dist/subscriber/**/*.js"
+  ],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+  },
+}
+
+export = config;
